c668dc30b3d3b5cf416bf420453144094268459e
rocarmo <rocarmo@pagseguro.com>
2021-07-07 18:59:35 -0300

    Updates

--- site_polls/api/data_collect.py
+++ site_polls/api/data_collect.py
@@ -1,37 +1,42 @@
 from jira import JIRA
 
 
+class DataImport:
+    def __init__(self):
+        # self.comments_qty = DataCollect.get_comments_qty
+        # self.comments_author = DataCollect.get_comments_author
+        # self.comments_date = DataCollect.get_comments_date
+        pass
+
 class DataCollect:
+    def __init__(self):
+        self.auth_jira = JIRA(options={'server': 'https://jiraps.atlassian.net/'},
+                              basic_auth=('rocarmo@uolinc.com', 'whSXrKnCX64Iei3rH4DO7136'))
+        self.issues_in_project = self.auth_jira.search_issues("project = 'PS - San Fierro' AND assignee = currentUser()")
 
-    def __init__(self, text1, text2):
-        self.text1 = text1
-        self.text2 = text2
 
+    def get_data(self):
 
-    # def get_data(self):
-    #     auth_jira = JIRA(options={'server': 'https://jiraps.atlassian.net/'},
-    #                      basic_auth=('rocarmo@uolinc.com', 'whSXrKnCX64Iei3rH4DO7136'))
-    #
-    #     print(auth_jira)
-    #
-    #     issues_in_project = auth_jira.search_issues("project = 'PS - San Fierro' AND assignee = currentUser()")
-    #
-    #     for issue in issues_in_project:
-    #         # print('-------------------------')
-    #         # print("Card: ", issue)
-    #         self.card_issue = issue
-    #
-    #         self.issue_coments = auth_jira.comments(issue)
-    #
-    #         # print('Comentários: ', len(issue_coments))
-    #         self.comments_qty = len(self.issue_coments)
-    #
-    #         for issue_coment in self.issue_coments:
-    #             # print('Autor do comentário: ', issue_coment.author.displayName)
-    #             self.comments_author = issue_coment.author.displayName
-    #
-    #             # print('Data de criação: ', issue_coment.created)
-    #             self.comments_date = issue_coment.created
-    #
-    #     # return self.comments_date, self.comments_author, self.comments_qty
+        for issue in self.issues_in_project:
+            self.issue_comments = self.auth_jira.comments(issue)
 
+    def get_comments_qty(self):
+        comments_qty = 12121212
+        # comments_qty = len(self.issue_comments)
+        return comments_qty
+
+    def get_comments_author(self):
+        # for issue_comment in self.issue_comments:
+        #     comments_author = issue_comment.author.displayName
+
+        comments_author = 'RODRIGOIDE PICA DAS GALAXIAS MANE'
+
+        return comments_author
+
+    def get_comments_date(self):
+
+        # for issue_comment_date in self.issue_comments:
+        #     comments_date = issue_comment_date.created
+
+        comments_date = '19/08/1997'
+        return comments_date
--- site_polls/views.py
+++ site_polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.renderers import JSONRenderer
-from site_polls.api.data_collect import DataCollect
+from site_polls.api.data_collect import DataImport
 from site_polls.models import DataReturn
 from site_polls.api.data_serializer import DataSerializer
 
@@ -12,12 +12,9 @@ def index(request):
 
 def get_data(request):
     if request.method == 'GET':
-        # send_data = DataReturn.objects.all()
-        data_collect = DataCollect(text1='FOI CARAI', text2='DISGRAÇAAAAAA')
+        data_collect = DataImport
         serializer = DataSerializer(data_collect)
         return JsonResponse(serializer.data, safe=False)
-        # return JSONRenderer().render(serializer.data)
-        # return HttpResponse(serializer.data)
 
     else:
         return HttpResponse("NÃO CAIU NO GET")
